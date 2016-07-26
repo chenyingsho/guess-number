bce7336c5bd2a39acc65a62f0db8b406640f0199
chenyingsho <601668970@qq.com>
2016-07-26 19:56:53 +0800

    create compareNumber

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -1,18 +1,34 @@
-/**
- * Created by chenying on 16-7-26.
- */
-const main = require('../main/main');
+'use strict'
+const CompareNumber = require('../main/modules/CompareNumber');
+describe('CompareNumber', ()=> {
+    const inputs = '1234';
 
-describe('printNumber', () => {
-
-    it('should print answer', () => {
-
-        const inputs = '1423';
-        const answer = '1324'
-
-
-        const expectText = '2A2B';
-
-        expect(main(answer,inputs)).toEqual(expectText);
+    it('if A and B all are 0', ()=> {
+        const systems = '5678';
+        const expectAnswer = '0A0B';
+        const answer = new CompareNumber(inputs, systems).print();
+        expect(answer).toEqual(expectAnswer);
     });
+
+    it('if B is 0', ()=> {
+        const systems = '1234';
+        const answer = new CompareNumber(inputs, systems).print();
+        const expectAnswer = '4A0B';
+        expect(answer).toEqual(expectAnswer);
+    });
+
+    it('if A is 0', ()=> {
+        const systems = '4321';
+        const expectAnswer = '0A4B';
+        const answer = new CompareNumber(inputs, systems).print();
+        expect(answer).toEqual(expectAnswer);
+    });
+
+    it('if A and B all are not 0', ()=> {
+        const systems = '1948';
+        const expectAnswer = '1A1B';
+        const answer = new CompareNumber(inputs, systems).print();
+        expect(answer).toEqual(expectAnswer);
+    })
 });
+
